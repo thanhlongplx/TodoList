2a5ea38acda852baf78099298fa562974ab741dc
letuantai97 <34785159+letuantai97@users.noreply.github.com>
2025-08-12 14:24:35 +0700

    login

--- app/src/main/java/com/example/todolist/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Frag"
+        value = "Đăng Nhập"
     }
     val text: LiveData<String> = _text
 }
